f0d007b9eff97553da06763549880f556e0ddae4
Vinay <imvinaytajane@gmail.com>
2022-02-20 11:57:27 +0530

    add space

--- game.py
+++ game.py
@@ -49,6 +49,7 @@ while turns > 0:
     # it will ask user to enter another alphabet
     # print()
     guess = input("guess a character:")
+    print("========================")
 
     # every input character will be stored in guesses
     guesses += guess
