4e2eb175d70f25bb68a522b3664b5ab0c5fd0fd0
Vinay <imvinaytajane@gmail.com>
2023-05-18 10:19:34 +0530

    modified game and expanded word list

--- game.py
+++ game.py
@@ -1,71 +1,135 @@
 import random
+import time
 
 name = input("What is your name? ")
-print("Good Luck !", name)
+print("Welcome, " + name + "! Let's play the word guessing game.")
 
 file = open('words.txt', 'r')
 words = file.read().split()
+file.close()
 
-# Function will choose one random and convert to lowercase
-# word from this list of words
-word = random.choice(words).lower()
-
-print("Guess the characters")
-
-guesses = ''
-
-turns = 10
-
-while turns > 0:
-
-    # counts the number of times a user fails
-    fail = 0
-
-    # all characters from the input
-    # word taking one at a time.
+def display_word(word, guesses):
     for char in word:
-
-        # comparing that character with
-        # the character in guesses
         if char in guesses:
             print(char, end=' ')
-
         else:
             print("_", end=' ')
-
-            # for every failure 1 will be incremented in failure
-            fail += 1
-
-    if fail == 0:
-        # user will win the game if failure is 0
-        # and 'You Win' will be given as output
-        print("You Win")
-
-        # this print the correct word
-        print("The word is:", word)
-        break
-
-    # if user has input the wrong alphabet then
-    # it will ask user to enter another alphabet
     print()
-    guess = input("\nguess a character:")
-    print("========================")
 
-    # every input character will be stored in guesses
-    guesses += guess
+def get_valid_guess(guesses):
+    while True:
+        guess = input("\nGuess a character: ").lower()
+        if len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single alphabetic character.")
+        elif guess in guesses:
+            print("You have already guessed that character. Try again.")
+        else:
+            return guess
 
-    # check input with the character in word
-    if guess not in word:
+def play_again():
+    while True:
+        choice = input("Do you want to play again? (yes/no): ").lower()
+        if choice == "yes":
+            return True
+        elif choice == "no":
+            return False
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
 
-        turns -= 1
+def display_hangman(turns):
+    hangman = [
+        '''
+          +---+
+              |
+              |
+              |
+             ===
+        ''',
+        '''
+          +---+
+          O   |
+              |
+              |
+             ===
+        ''',
+        '''
+          +---+
+          O   |
+          |   |
+              |
+             ===
+        ''',
+        '''
+          +---+
+          O   |
+         /|   |
+              |
+             ===
+        ''',
+        '''
+          +---+
+          O   |
+         /|\\  |
+              |
+             ===
+        ''',
+        '''
+          +---+
+          O   |
+         /|\\  |
+         /    |
+             ===
+        ''',
+        '''
+          +---+
+          O   |
+         /|\\  |
+         / \\  |
+             ===
+        '''
+    ]
+    max_turns = len(hangman) - 1
+    display_stage = min(turns, max_turns)
+    print(hangman[display_stage])
 
-        # if the character doesn’t match the word
-        # then “Wrong” will be given as output
-        print("Wrong")
 
-        # this will print the number of
-        # turns left for the user
-        print("You have", + turns, 'more guesses')
+# Game loop
+play_game = True
+while play_game:
+    # Choose a random word
+    word = random.choice(words).lower()
+    word_length = len(word)
 
-        if turns == 0:
-            print("You Loose"), print("Word is:", word)
+    print("\nLet's begin! The word has", word_length, "characters.")
+
+    guesses = []
+    turns = 7
+    score = 0
+
+    while turns > 0:
+        print("\nCurrent Score:", score)
+        display_word(word, guesses)
+        display_hangman(turns)
+
+        guess = get_valid_guess(guesses)
+        guesses.append(guess)
+
+        if guess in word:
+            print("Correct guess!")
+            score += 1
+        else:
+            print("Wrong guess!")
+            turns -= 1
+
+        if all(char in guesses for char in word):
+            print("Congratulations, you won!")
+            print("The word is:", word)
+            break
+
+    if turns == 0:
+        print("Oops! You ran out of turns.")
+        print("The word was:", word)
+
+    play_game = play_again()
+
+print("Thank you for playing. Goodbye!")
